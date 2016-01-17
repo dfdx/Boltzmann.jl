ee75009080d5f1211c7daea7d98565c32132b4e1
Andrei Zhabinski <faithlessfriend@gmail.com>
2016-01-17 03:02:36 +0300

    general idease

--- src/rbm2.jl
+++ src/rbm2.jl
@@ -1,12 +1,22 @@
 
 include("utils.jl")
 
+using Base.LinAlg.BLAS
+using Distributions
+import StatsBase.fit
+
+typealias Mat{T} AbstractArray{T, 2}
+typealias Vec{T} AbstractArray{T, 1}
+
+
 abstract AbstractRBM
 
 @runonce type RBM{V,H} <: AbstractRBM
-    W::Matrix{Float64}
-    vbias::Vector{Float64}
-    hbias::Vector{Float64}
+    W::Matrix{Float64}        # matrix of weights between vis and hid vars
+    vbias::Vector{Float64}    # biases for visible variables
+    hbias::Vector{Float64}    # biases for hidden variables
+    cache::Dict{Symbol, Any}  # cache for objects used during fitting
+                              #  (e.g. previous weights)
 end
 
 function RBM(V::Type, H::Type,
@@ -21,13 +31,180 @@ function Base.show{V,H}(io::IO, rbm::RBM{V,H})
     print(io, "RBM{$V,$H}($n_vis, $n_hid)")
 end
 
+## utils
 
-function persistent_contdiv()
+function logistic(x)
+    return 1 ./ (1 + exp(-x))
 end
+
+
+function hid_means(rbm::RBM, vis::Mat{Float64})
+    p = rbm.W * vis .+ rbm.hbias
+    return logistic(p)
+end
+
+
+function vis_means(rbm::RBM, hid::Mat{Float64})
+    p = rbm.W' * hid .+ rbm.vbias
+    return logistic(p)
+end
+
+
+## samping
+
+function sample(::Type{:mean}, means::Mat{Float64})
+    return means
+end
+
+function sample(::Type{:bernoulli}, means::Mat{Float64})
+    return float(rand(size(means)) .< means)
+end
+
+
+function sample(::Type{:gaussian}, means::Mat{Float64})
+    sigma2 = 1                   # using fixed standard diviation
+    samples = zeros(size(means))
+    for j=1:size(means, 2), i=1:size(means, 1)
+        samples[i, j] = rand(Normal(means[i, j], sigma2))
+    end
+    return samples
+end
+
+
+function sample_hiddens{V,H}(rbm::RBM{V, H}, vis::Mat{Float64})
+    means = hid_means(rbm, vis)
+    return sample(H, means)
+end
+
+
+function sample_visibles{V,H}(rbm::RBM{V,H}, hid::Mat{Float64})
+    means = vis_means(rbm, hid)
+    return sample(V, means)
+end
+
+
+function gibbs(rbm::RBM, vis::Mat{Float64}; n_times=1)
+    v_pos = vis
+    h_pos = sample_hiddens(rbm, v_pos)
+    v_neg = sample_visibles(rbm, h_pos)
+    h_neg = sample_hiddens(rbm, v_neg)
+    for i=1:n_times-1
+        v_neg = sample_visibles(rbm, h_neg)
+        h_neg = sample_hiddens(rbm, v_neg)
+    end
+    return v_pos, h_pos, v_neg, h_neg
+end
+
+
+## scoring
+
+function free_energy(rbm::RBM, vis::Mat{Float64})
+    vb = sum(vis .* rbm.vbias, 1)
+    Wx_b_log = sum(log(1 + exp(rbm.W * vis .+ rbm.hbias)), 1)
+    return - vb - Wx_b_log
+end
+
+
+function score_samples(rbm::AbstractRBM, vis::Mat{Float64}; sample_size=10000)
+    if issparse(vis)
+        # sparse matrices may be infeasible for this operation
+        # so using only little sample
+        cols = rand(1:size(vis, 2), sample_size)
+        vis = full(vis[:, cols])
+    end
+    n_feat, n_samples = size(vis)
+    vis_corrupted = copy(vis)
+    idxs = rand(1:n_feat, n_samples)
+    for (i, j) in zip(idxs, 1:n_samples)
+        vis_corrupted[i, j] = 1 - vis_corrupted[i, j]
+    end
+    fe = free_energy(rbm, vis)
+    fe_corrupted = free_energy(rbm, vis_corrupted)
+    return n_feat * log(logistic(fe_corrupted - fe))
+end
+
+
+## gradient calculation
+
+function contdiv(rbm::AbstractRBM, vis::Mat{Float64}, n_gibbs::Int)
+    v_pos, h_pos, v_neg, h_neg = gibbs(rbm, vis, n_times=n_gibbs)
+    return v_pos, h_pos, v_neg, h_neg
+end
+
+
+function persistent_contdiv(rbm::AbstractRBM, vis::Mat{Float64}, n_gibbs::Int)
+    if size(rbm.persistent_chain) != size(vis)
+        # persistent_chain not initialized or batch size changed, re-initialize
+        rbm.persistent_chain = vis
+    end
+    # take positive samples from real data
+    v_pos, h_pos, _, _ = gibbs(rbm, vis)
+    # take negative samples from "fantasy particles"
+    rbm.persistent_chain, _, v_neg, h_neg = gibbs(rbm, vis, n_times=n_gibbs)
+    return v_pos, h_pos, v_neg, h_neg
+end
+
+
+function gradient()
+    dW = buf
+    # dW = (h_pos * v_pos') - (h_neg * v_neg')
+    gemm!('N', 'T', lr, h_neg, v_neg, 0.0, dW)
+    gemm!('N', 'T', lr, h_pos, v_pos, -1.0, dW)
+    return dW
+end
+
+## updating
+
+function update_weights!(rbm, h_pos, v_pos, h_neg, v_neg, lr, buf)    
+    # rbm.dW += rbm.momentum * rbm.dW_prev
+    axpy!(rbm.momentum, rbm.dW_prev, dW)
+    # rbm.W += lr * dW
+    axpy!(1.0, dW, rbm.W)
+    # save current dW
+    copy!(rbm.dW_prev, dW)
+end
+
 
 function default_update!()
 end
 
+
+## fitting
+
+function fit_batch!(rbm::RBM, vis::Mat{Float64};
+                    persistent=true, buf=nothing, lr=0.1, n_gibbs=1)
+    buf = buf == nothing ? zeros(size(rbm.W)) : buf
+    # v_pos, h_pos, v_neg, h_neg = gibbs(rbm, vis, n_times=n_gibbs)
+    sampler = persistent ? persistent_contdiv : contdiv
+    v_pos, h_pos, v_neg, h_neg = sampler(rbm, vis, n_gibbs)
+    lr=lr/size(v_pos,2)
+    update_weights!(rbm, h_pos, v_pos, h_neg, v_neg, lr, buf)
+    rbm.hbias += vec(lr * (sum(h_pos, 2) - sum(h_neg, 2)))
+    rbm.vbias += vec(lr * (sum(v_pos, 2) - sum(v_neg, 2)))
+    return rbm
+end
+
+
+## function fit(rbm::RBM, X::Mat{Float64};
+##              persistent=true, lr=0.1, n_iter=10, batch_size=100, n_gibbs=1)
+##     @assert minimum(X) >= 0 && maximum(X) <= 1
+##     n_samples = size(X, 2)
+##     n_batches = @compat Int(ceil(n_samples / batch_size))
+##     w_buf = zeros(size(rbm.W))
+##     for itr=1:n_iter
+##         tic()
+##         for i=1:n_batches
+##             batch = X[:, ((i-1)*batch_size + 1):min(i*batch_size, end)]
+##             batch = full(batch)
+##             fit_batch!(rbm, batch, persistent=persistent,
+##                        buf=w_buf, n_gibbs=n_gibbs)
+##         end
+##         toc()
+##         pseudo_likelihood = mean(score_samples(rbm, X))
+##         info("Iteration #$itr, pseudo-likelihood = $pseudo_likelihood")
+##     end
+##     return rbm
+## end
 
 function fit(rbm::RBM, X::Matrix{Float64};
              gradient=persistent_contdiv,update! = default_update!, opts...)
@@ -35,3 +212,24 @@ function fit(rbm::RBM, X::Matrix{Float64};
 end
 
 
+## operations on learned RBM
+
+function transform(rbm::RBM, X::Mat{Float64})
+    return hid_means(rbm, X)
+end
+
+
+function generate(rbm::RBM, vis::Vec{Float64}; n_gibbs=1)
+    return gibbs(rbm, reshape(vis, length(vis), 1); n_times=n_gibbs)[3]
+end
+
+function generate(rbm::RBM, X::Mat{Float64}; n_gibbs=1)
+    return gibbs(rbm, X; n_times=n_gibbs)[3]
+end
+
+
+function components(rbm::AbstractRBM; transpose=true)
+    return if transpose rbm.W' else rbm.W end
+end
+# synonym
+weights(rbm::AbstractRBM; transpose=true) = components(rbm, transpose)
