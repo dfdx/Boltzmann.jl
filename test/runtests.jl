91ed5227e7648f70ec1ed5acecf38620d001d84d
Rory Finnegan <rory.finnegan@gmail.com>
2016-03-09 13:22:13 -0500

    Initial refactoring of test cases.
    1. Added a hacky solution using environment variables to allow running
       acceptance tests and benchmarks separately. This is to address how
       slow running all tests with `coverage=true`
    2. Added some basic benchmarking code. Not sure if we should try and do
       some automated comparisons.
    3. Using BaseTestNext to get `@testsets`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,40 @@
+if VERSION >= v"0.5-"
+    using Base.Test
+else
+    using BaseTestNext
+    const Test = BaseTestNext
+end
 
-include("testutils.jl")
-include("testrbm.jl")
-include("testnets.jl")
-include("testconditional.jl")
+using Boltzmann
+using Benchmark
+using DataFrames    # Needed for organization of benchmark results
+
+#=
+Available test groups are:
+* :unit,
+* :integration,
+* :acceptance,
+* :benchmark,
+=#
+const DEFAULT_TEST_GROUPS = Set([:unit, :integration])
+TEST_GROUPS = DEFAULT_TEST_GROUPS
+
+# If the JULIA_TEST_GROUPS ENV variable is set
+# then use the inte
+if haskey(ENV, "JULIA_TEST_GROUPS")
+    TEST_GROUPS = Set(
+        Symbol[split(ENV["JULIA_TEST_GROUPS"])...]
+    )
+end
+
+tests = [
+    "utils",
+    "rbm",
+    "nets",
+    "conditional",
+]
+
+for t in tests
+    println("\n\nRunning $t tests...\n")
+    include(string("test", t, ".jl"))
+end
--- test/testconditional.jl
+++ test/testconditional.jl
@@ -4,40 +4,6 @@ using Base.Test
 using MNIST
 
 
-function conditional_smoke_test()
-    X = rand(1000, 200)
-    model = ConditionalRBM(Bernoulli, Bernoulli, 250, 150; steps=3)
-    fit(model, X; n_epochs=10, n_gibbs=5)
-    forecast = predict(model, X[251:1000, 1]; n_gibbs=5)
-end
-
-function run_mnist()
-    train_X = _get_dataset(traindata()[1][:, 1:10000])
-    input_size = round(Int, size(train_X, 1) / 2)
-
-    model = ConditionalRBM(Bernoulli, Bernoulli, input_size, 500; steps=1)
-    fit(model, train_X; n_epochs=10, n_gibbs=5)
-
-    test_X = _get_dataset(testdata()[1][:, 1:1000])
-    corrupt_start = input_size + 1
-    forecast = predict(model, test_X[corrupt_start:end,:]; n_gibbs=20)
-
-    mae = mean(test_X[1:corrupt_start-1,:] .!= forecast)
-
-    # At a bare minimum the prediction should have an mae
-    # less than the amount of noise added to the condition
-    # values. In reality, this should be around ~0.05
-    # and the likelihood should be move from >-60 to <-31
-    @test mae < 0.1
-
-    @test !any(isnan, model.W)
-    @test !any(isnan, model.A)
-    @test !any(isnan, model.B)
-    @test !any(isinf, model.W)
-    @test !any(isinf, model.A)
-    @test !any(isinf, model.B)
-end
-
 function _get_dataset(X)
     X = round(X ./ (maximum(X) - minimum(X)))
     X_c = copy(X)   # the corrupted images
@@ -55,5 +21,73 @@ function _get_dataset(X)
     return vcat(X, X_c)
 end
 
-conditional_smoke_test()
-run_mnist()
+
+if :integration in TEST_GROUPS
+    @testset "Conditional RBM Integration" begin
+        X = rand(1000, 200)
+        model = ConditionalRBM(Bernoulli, Bernoulli, 250, 150; steps=3)
+        fit(model, X; n_epochs=10, n_gibbs=5)
+        forecast = predict(model, X[251:1000, 1]; n_gibbs=5)
+    end
+end
+
+if :acceptance in TEST_GROUPS
+    @testset "Conditional RBM Acceptance" begin
+        train_X = _get_dataset(traindata()[1][:, 1:10000])
+        input_size = round(Int, size(train_X, 1) / 2)
+
+        model = ConditionalRBM(Bernoulli, Bernoulli, input_size, 500; steps=1)
+        fit(model, train_X; n_epochs=10, n_gibbs=5)
+
+        test_X = _get_dataset(testdata()[1][:, 1:1000])
+        corrupt_start = input_size + 1
+        forecast = predict(model, test_X[corrupt_start:end,:]; n_gibbs=20)
+
+        mae = mean(test_X[1:corrupt_start-1,:] .!= forecast)
+
+        # At a bare minimum the prediction should have an mae
+        # less than the amount of noise added to the condition
+        # values. In reality, this should be around ~0.05
+        # and the likelihood should be move from >-60 to <-31
+        @test mae < 0.1
+
+        @test !any(isnan, model.W)
+        @test !any(isnan, model.A)
+        @test !any(isnan, model.B)
+        @test !any(isinf, model.W)
+        @test !any(isinf, model.A)
+        @test !any(isinf, model.B)
+    end
+end
+
+if :benchmark in TEST_GROUPS
+    @testset "Conditional RBM Benchmark" begin
+        n_vis = 250
+        n_cond = 750
+        n_hid = 200
+        n_obs = 300
+
+        df = DataFrame()
+
+        for T in [Float32, Float64]
+            X = rand(T, n_vis + n_cond, n_obs)
+            rbm = ConditionalRBM(T, Bernoulli, Bernoulli, n_vis, n_hid, n_cond)
+
+            # For now we're just benchmarking the fit method
+            # but we could also get transform, generate, etc
+            fit_func() = fit(rbm, X)
+            result = benchmark(fit_func, "fit", "$(typeof(X))", 10)
+
+            if isempty(df)
+                df = result
+            else
+                df = vcat(df, result)
+            end
+        end
+
+        # For now just print the output, but we may want load a CSV containing
+        # existing master benchmarks and compare the two here.
+        println(df)
+    end
+end
+
